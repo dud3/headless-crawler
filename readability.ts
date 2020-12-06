25f00c0acae9dc649d70a94168cade2a6dcc3b05
dud3 <dk18184@gmail.com>
2020-12-06 19:15:27 +0100

    refactor: few lines

--- readability.ts
+++ readability.ts
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 import { Page } from "puppeteer";
 
-const readabilityStr = readFileSync('node_modules/@mozilla/readability/Readability.js', {encoding: 'utf-8'})
+const readabilityStr = readFileSync('node_modules/@mozilla/readability/Readability.js', {encoding: 'utf-8'});
 const { Readability } = require('@mozilla/readability');
 
 function rexecutor() {
@@ -11,21 +11,6 @@ function rexecutor() {
 }
 
 async function main (page: { page: Page; url: string }) {
-  /*
-    await page.removeAllListeners('request');
-    await page.setRequestInterception(true);
-    page.on('request', (request) => {
-      if (request.resourceType() == 'media'
-        || request.resourceType() == 'image'
-        || request.resourceType() == 'stylesheet'
-        || request.resourceType() == 'font') {
-          request.abort();
-      } else {
-          request.continue();
-      }
-  });
-  */
-
   await page.page.goto(page.url, { timeout: 0, waitUntil: 'domcontentloaded' });
 
   const read = await page.page.evaluate(`
