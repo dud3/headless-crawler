81e5b44a7b6409ccf810d5f94b56318ea2d622d5
dud3 <dk18184@gmail.com>
2020-12-28 21:36:26 +0100

    task: notes

--- db-sql.ts
+++ db-sql.ts
@@ -2,14 +2,23 @@ require('dotenv').config()
 
 var mysql = require('mysql');
 
+// note: after creating the user (in my case remoteuser0) execute the following:
+// execute ALTER USER 'remoteuser0'@'%' IDENTIFIED WITH mysql_native_password BY 'password'
+// otherwise the code below will fail
+
 var con = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: 3306,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   charset : 'utf8mb4_unicode_ci'
 })
 
-con.connect();
+con.connect((err) => {
+  if (err) throw err;
+
+  console.log('Connected to the MySQL server.');
+});
 
 export default con;
