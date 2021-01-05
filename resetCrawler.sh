2d19adef6e9b206f3c43c15376c5fc9b5003216d
dud3 <dk18184@gmail.com>
2021-01-05 18:52:34 +0000

    ...

--- resetCrawler.sh
+++ resetCrawler.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 cd /root/headless-crawler
 forever stopall
 pkill chrome
 pkill node
-service mysql restart
-forever start -c "yarn shell --tabs=10 --waitfor=3000 --sites=47350" ./
+# service mysql restart
+forever start -c "yarn shell --tabs=4 --waitfor=3000 --closetabs=true" ./
 forever list
